7bde3fcdbcf69dac3c89dbd219a486aba7867808
saleena <saleenamb21@gmail.com>
2024-08-05 15:08:55 +0530

    first commit

---
+++ new_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request,'view.html')
+def index(request):
+    return render(request,'index.html')
+
+def dash(request):
+    return render(request,'dash.html')
