13e157a667226b409e75e2656738929a7227202c
Nandhesh <nandhesh45@gmail.com>
2024-08-07 16:56:47 +0530

    crud commit

--- new_app/urls.py
+++ new_app/urls.py
@@ -4,8 +4,10 @@ from new_app import views
 
 urlpatterns = [
     path("home",views.home,name="home"),
-    path("index",views.index,name="index"),
+    path("",views.index,name="index"),
     path("dash",views.dash,name="dash"),
-    path("data",views.furniture,name="furniture"),
-    path("table",views.furniture_view,name="furniture_view")
+    path("data",views.furniture,name="data"),
+    path("table",views.furniture_view,name="table"),
+    path("delete/<int:id>/",views.furniture_delete,name="furniture_delete"),
+    path("update/<int:id>/",views.furniture_update,name='furniture_update')
 ]
--- new_app/views.py
+++ new_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from new_app.forms import Furniture, FurnitureForm
 
@@ -22,3 +22,18 @@ def furniture(request):
 def furniture_view(request):
     data = Furniture.objects.all()
     return render(request,'view_data.html', {'data': data})
+
+def furniture_delete(request,id):
+    data = Furniture.objects.get(id=id)
+    data.delete()
+    return redirect("furniture_view")
+
+def furniture_update(request,id):
+    data = Furniture.objects.get(id=id)
+    form = FurnitureForm(instance= data)
+    if request.method == 'POST':
+        data = FurnitureForm(request.POST,instance=data)
+        if data.is_valid():
+            data.save()
+            return redirect("furniture_view")
+    return render(request,'update.html',{'form':form})
